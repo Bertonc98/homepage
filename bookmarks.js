de80f14d3c696bb050e1d5c6699fe69174c60378
RicoPallazzo <43791854+Bertonc98@users.noreply.github.com>
2024-08-09 17:47:57 +0200

    Update bookmarks.js

--- bookmarks.js
+++ bookmarks.js
@@ -28,7 +28,7 @@ const bookmarks = [
     links: [
       { name: "Whatsapp", url: "https://web.whatsapp.com/" },
       { name: "Telegram", url: "https://web.telegram.org/k/" },
-      { name: "Facebook", url: "https://reddit.com" },
+      { name: "Reddit", url: "https://reddit.com" },
       { name: "Instagram", url: "https://www.instagram.com/" },
       { name: "Facebook", url: "https://facebook.com" },
     ],
